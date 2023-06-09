723368f9b54533d2ec45e91bcb7d37ea0fab8815
GabSiccion <gabsiccion@outlook.com>
2023-06-10 07:22:49 +0800

    1st chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,17 @@
 import "../index.css";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, queryEqual } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db, auth } from "../config/Firebase";
+import { db, auth, coursesCollectionRef } from "../config/Firebase";
 import { getCountFromServer, query, where } from "firebase/firestore";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function Dashboard() {
   const [courseList, setCourseList] = useState([]);
-  const [countList, setCountList] = useState([]);
-  const [averageScores, setAverageScores] = useState([]);
+  const [scores, setScores] = useState([]);
 
   const scoresColl = collection(db, "scores");
 
@@ -23,39 +25,84 @@ export function Dashboard() {
     async function getCourses() {
       let courseArray = [];
       const querySnapshot = await getDocs(collection(db, "courses"));
-      querySnapshot.forEach(async (doc) => {
+
+      querySnapshot.forEach((doc) => {
         courseArray.push(doc.get("courseName"));
       });
       setCourseList(courseArray.slice());
     }
     getCourses();
 
-    async function getCount2() {
-      let countArray = [];
-      courseList.map((courseName) => {
-        getCount(courseName).then((value) => {
-          countArray.push(value);
-        });
+    async function getScores() {
+      let scoresArray = [];
+      const querySnapshot = await getDocs(collection(db, "scores"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        scoresArray.push(doc.data());
       });
-      setCountList(await countArray);
+      setScores(scoresArray.slice());
     }
-    getCount2();
+    getScores();
   }, []);
+
+  //DISTRIBUTION CHART
+  const pie = () => {
+    let courseScores = [];
+    scores.map((score) => {
+      if (score["course"] === "Information Technology") {
+        courseScores.push(score["recommendation"]);
+      }
+    });
+    let coursetracks = courseScores.filter(function (item, pos) {
+      return courseScores.indexOf(item) == pos;
+    });
+
+    console.log(coursetracks);
+    console.log(courseScores);
+
+    let distribution = [];
+    coursetracks.map((track, index) => {
+      let instances = 0;
+      courseScores.map((scoreTrack) => {
+        if (track === scoreTrack) instances++;
+      });
+      distribution[index] = instances;
+    });
+
+    console.log(distribution);
+
+    const distributionData = {
+      labels: coursetracks,
+      datasets: [
+        {
+          label: "Distribution of track recommendation",
+          data: distribution,
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 206, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(153, 102, 255)",
+            "rgb(255, 159, 64)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 206, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(153, 102, 255)",
+            "rgb(255, 159, 64)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    return distributionData;
+  };
 
   return (
     <div className="container mt-4">
-      <div className="row">
-        <div className="col-6 d-flex flex-column justify-content-center">
-          <h1>
-            {countList.reduce(function (a, b) {
-              return a + b;
-            }, 0)}
-          </h1>
-          <p className="fs-4">Times the quiz has been answered.</p>
-        </div>
-        <div className="col-6"></div>
-        <div className="col-6"></div>
-      </div>
+      <Pie data={pie()} />
     </div>
   );
 }
