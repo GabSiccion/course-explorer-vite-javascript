c0a2e6b252333d0eba9d2f065e79cb32b9cbb8ff
GabSiccion <gabsiccion@outlook.com>
2023-06-09 08:43:37 +0800

    Account Creation

--- src/components/AccountManagement.jsx
+++ src/components/AccountManagement.jsx
@@ -1,9 +1,59 @@
 import "../index.css";
+import { auth } from "../config/Firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { AuthErrorCodes } from "firebase/auth";
+import { useState } from "react";
 
 export function AccountManagement() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const signUpNewUser = async () => {
+    //confirm password
+    if (password.length >= 6) {
+      if (password !== confirmPassword) {
+        alert("password and confirm password are not the same");
+      } else {
+        try {
+          await createUserWithEmailAndPassword(auth, email, password).catch(
+            (reason) => {
+              alert(reason);
+            }
+          );
+        } catch (err) {
+          console.log(AuthErrorCodes[err]);
+        }
+      }
+    } else {
+      alert("password too short, minimum of 5 characters");
+    }
+  };
+
   return (
     <div className="page-container">
-      <h1>Account Management</h1>
+      <h1>Register a new user</h1>
+      <div className="sign-up-container">
+        <input
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          placeholder="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button onClick={signUpNewUser}>Create Account</button>
+      </div>
     </div>
   );
 }
